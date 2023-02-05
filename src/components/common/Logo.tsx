f3d31523f0725687f7d8f18987498f883612ad3f
Stanley Pierre-Louis <stanleypl75@gmail.com>
2023-02-04 21:01:40 -0400

    feat: add logo

---
+++ src/components/common/Logo.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Image from 'next/image';
+import styled from 'styled-components';
+
+import image from '../../../public/logo.svg';
+
+const WrapperImage = styled.div`
+  margin: 5px;
+`;
+
+export default function Logo() {
+  return (
+    <WrapperImage>
+      <Image src={image} alt="logo" width={100} height={100} />
+    </WrapperImage>
+  );
+}
