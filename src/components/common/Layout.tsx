b261d94191b4c5b31c66a3655deb2a7d2f9d216f
Stanley Pierre-Louis <stanleypl75@gmail.com>
2023-02-05 21:10:32 -0400

    fix: add some new styles

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,19 +1,23 @@
 import styled from 'styled-components';
 
+import Footer from '../footer/Footer';
 import Header from '../navbar/Navbar';
 
 const Container = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
-  height: auto;
-  max-width: 50%;
+  min-height: 100vh;
+  justify-content: space-between;
+  margin-left: auto;
+  margin-right: auto;
 `;
 export default function Layout({ children }: any) {
   return (
     <Container>
       <Header />
       {children}
+      <Footer />
     </Container>
   );
 }
