2394ae6a8ed5f7906f4563e5b4ac523b964553de
Stanley Pierre-Louis <stanleypl75@gmail.com>
2023-02-07 13:36:12 -0400

    test: update footer test

--- src/components/common/footer/Footer.cy.tsx
+++ src/components/common/footer/Footer.cy.tsx
@@ -3,13 +3,14 @@ import Footer from '@/components/common/footer/Footer';
 import { GlobalTheme } from '@/components/common/navbar/Navbar.cy';
 
 describe('<Footer/>', () => {
+  const date = new Date().getFullYear();
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
     cy.mount(
       <GlobalTheme>
-        <Footer />
+        <Footer date={date} />
       </GlobalTheme>
     );
-    cy.get('a').should('have.length', 3);
+    cy.get('p').should('have.text', `© ${date} YayaResto `);
   });
 });
