84e83d152ae099cb468b61e8b61325dce25a762c
Stanley Pierre-Louis <stanleypl75@gmail.com>
2023-02-07 13:47:12 -0400

    style: update styles

--- src/components/common/navbar/List.tsx
+++ src/components/common/navbar/List.tsx
@@ -4,49 +4,24 @@ import styled from 'styled-components';
 import { ListProps } from '@/utils/types';
 
 const ListItem = styled.li`
-  padding: 10px;
   text-transform: uppercase;
   font-size: 18px;
   list-style-type: none;
   color: ${(props) => props.theme.colors.white};
-
-  @media (max-width: 845px) {
-    font-size: 18px;
-  }
+  font-family: 'Poppins', sans-serif;
 `;
 const NextLink = styled(Link)<ListProps>`
   color: ${(props) => props.theme.colors.white};
   display: block;
-`;
-
-const Button = styled.button`
-  border: 1px solid #fff;
-  background: ${(props) => props.theme.colors.black};
-  border-radius: 5px;
-  padding: 5px 10px;
+  padding: 1em;
 `;
 
 const List: React.FC = () => {
-  const array = [
-    'Home',
-    'Menu',
-    'About us',
-    'Our Story',
-    'Blog',
-    'Contact',
-    'Login',
-  ];
+  const array = ['Home', 'Menu', 'About us', 'Our Story', 'Blog', 'Contact'];
   return (
     <>
       {array.map((list, i) => (
         <ListItem key={i}>
-          {list === 'Login' && (
-            <Button>
-              <NextLink href={`${list}`} list={list}>
-                {list.toUpperCase()}
-              </NextLink>
-            </Button>
-          )}
           <NextLink href={`${list}`} list={list}>
             {list !== 'Login' && list.toUpperCase()}
           </NextLink>
