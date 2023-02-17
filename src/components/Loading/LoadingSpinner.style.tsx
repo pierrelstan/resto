d50b952dae553239e05c4eff8adf70f8b0f0b2a7
Stanley Pierre-Louis <stanleypl75@gmail.com>
2023-02-17 06:24:25 -0400

    style: loading style

---
+++ src/components/Loading/LoadingSpinner.style.tsx
@@ -0,0 +1,8 @@
+import { TailSpin } from 'react-loader-spinner';
+import styled from 'styled-components';
+
+export const Loading = styled(TailSpin)``;
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
--- src/components/Loading/LoadingSpinner.tsx
+++ src/components/Loading/LoadingSpinner.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import { TailSpin } from 'react-loader-spinner';
-import styled from 'styled-components';
+import * as S from './LoadingSpinner.style';
 
 type Props = {};
-const Loading = styled(TailSpin)``;
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-`;
 
 const LoadingSpinner = (props: Props) => {
   return (
-    <Container>
-      <Loading
+    <S.Container>
+      <S.Loading
         visible={true}
         height="80"
         width="80"
@@ -21,7 +15,7 @@ const LoadingSpinner = (props: Props) => {
         wrapperClass="blocks-wrapper"
         color="#FFB577"
       />
-    </Container>
+    </S.Container>
   );
 };
 
