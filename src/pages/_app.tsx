c31f681e56804a0f5bebdba8800067066c043aa2
Stanley Pierre-Louis <stanleypl75@gmail.com>
2023-02-24 15:15:36 -0400

    chore: update nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,34 +10,29 @@ import GlobalStyle from '../styles/globalstyles';
 import theme from '../styles/theme';
 import 'nprogress/nprogress.css'; //styles of nprogress
 
-//Binding events.
-Router.events.on('routeChangeStart', () => NProgress.start());
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
-
 const playfair_Display = Playfair_Display({
   weight: '400',
   subsets: ['latin'],
 });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  // React.useEffect(() => {
-  //   const handleRouteStart = () => {
-  //     NProgress.start();
-  //   };
-  //   const handleRouteDone = () => NProgress.done();
+  React.useEffect(() => {
+    const handleRouteStart = () => {
+      NProgress.start();
+    };
+    const handleRouteDone = () => NProgress.done();
 
-  //   Router.events.on('routeChangeStart', handleRouteStart);
-  //   Router.events.on('routeChangeComplete', handleRouteDone);
-  //   Router.events.on('routeChangeError', handleRouteDone);
+    Router.events.on('routeChangeStart', handleRouteStart);
+    Router.events.on('routeChangeComplete', handleRouteDone);
+    Router.events.on('routeChangeError', handleRouteDone);
 
-  //   return () => {
-  //     // Make sure to remove the event handler on unmount!
-  //     Router.events.off('routeChangeStart', handleRouteStart);
-  //     Router.events.off('routeChangeComplete', handleRouteDone);
-  //     Router.events.off('routeChangeError', handleRouteDone);
-  //   };
-  // }, []);
+    return () => {
+      // Make sure to remove the event handler on unmount!
+      Router.events.off('routeChangeStart', handleRouteStart);
+      Router.events.off('routeChangeComplete', handleRouteDone);
+      Router.events.off('routeChangeError', handleRouteDone);
+    };
+  }, []);
 
   return (
     <Provider store={store}>
