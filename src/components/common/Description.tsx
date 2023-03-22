e465625193c1a4819562921e40543d7c75bb7c55
Stanley Pierre-Louis <stanleypl75@gmail.com>
2023-03-22 18:37:55 -0400

    fix: add condition

--- src/components/common/Description.tsx
+++ src/components/common/Description.tsx
@@ -1,8 +1,26 @@
+import React from 'react';
 import * as SharedStyles from '@/styles/sharedstyles';
 interface IDescriptionProps {
-  description: string;
+  description: Array<string> | string | undefined;
 }
 const Description: React.FC<IDescriptionProps> = (props) => {
-  return <SharedStyles.Text>{props.description}</SharedStyles.Text>;
+  return (
+    <React.Fragment>
+      {Array.isArray(props.description) === false && (
+        <SharedStyles.Text>{props.description}</SharedStyles.Text>
+      )}
+
+      {Array.isArray(props.description) === true && (
+        <>
+          {Array.isArray(props.description) &&
+            props.description.map((text: string, i: number) => (
+              <React.Fragment key={i}>
+                <SharedStyles.Text>{text}</SharedStyles.Text>
+              </React.Fragment>
+            ))}
+        </>
+      )}
+    </React.Fragment>
+  );
 };
 export default Description;
