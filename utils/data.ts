519a59b5eae1304571e4974dc50ec577e1998ce6
Stanley Pierre-Louis <stanleypl75@gmail.com>
2023-03-22 18:38:43 -0400

    fix: update texts

--- utils/data.ts
+++ utils/data.ts
@@ -6,7 +6,7 @@ const data = {
       title: 'We serve high quality foods of all kinds.',
       subTitle: false,
       description:
-        'Through True Rich Attended does no end it his mother since favourable real had half every him case in packages enquire we up ecstatic.Through True Rich Attended does no end it his mother',
+        'Our dedication to excellence is central to everything we do. Come join us for dinner and experience the difference that high-quality ingredients and skilled preparation can make.',
     },
   ],
   two: [
@@ -22,7 +22,7 @@ const data = {
       color: 'white',
       title: 'We source sustainable & line caught Foods',
       description:
-        'Through True Rich Attended does no end it his mother since favourable real had half every him case in packages enquire we up ecstatic.Through True Rich Attended does no end it his mother',
+        'At our restaurant, we believe, starts with great ingredients. That is why we have committed to sourcing only the highest-quality, sustainably grown, and line-caught foods for our dishes.',
       subTitle: true,
     },
   ],
@@ -39,7 +39,7 @@ const data = {
       color: 'white',
       title: 'We all have to eat, so Why not do it beautifully?',
       description:
-        'Through True Rich Attended does no end it his mother since favourable real had half every him case in packages enquire we up ecstatic.Through True Rich Attended does no end it his mother',
+        "Eating is more than just a requirement; it is also a pleasurable experience. At our restaurant, we believe that every dinner should be a beautiful, memorable moment in time. The moment you step through our doors, you'll be transported to a world of gastronomic delight, where the ambiance is as carefully crafted as the menu.",
       subTitle: true,
     },
   ],
@@ -56,7 +56,7 @@ const data = {
       color: 'white',
       title: 'Discover our menu chart',
       description:
-        'Through True Rich Attended does no end it his mother since favourable real had half every him case in packages enquire we up ecstatic.Through True Rich Attended does no end it his mother',
+        'From juicy burgers and sizzling steaks to refreshing cocktails and decadent desserts, our menu has something for everyone. So come and explore our carefully crafted dishes, made with the freshest ingredients and bursting with flavor. Your next dining experience awaits!',
       subTitle: true,
     },
   ],
@@ -73,7 +73,7 @@ const data = {
       color: 'white',
       title: 'Discover our menu chart',
       description:
-        'Through True Rich Attended does no end it his mother since favourable real had half every him case in packages enquire we up ecstatic.Through True Rich Attended does no end it his mother',
+        'From juicy burgers and sizzling steaks to refreshing cocktails and decadent desserts, our menu has something for everyone. So come and explore our carefully crafted dishes, made with the freshest ingredients and bursting with flavor. Your next dining experience awaits!',
       subTitle: true,
     },
   ],
@@ -83,16 +83,21 @@ const data = {
       color: 'white',
       title: 'We are one of the best Cafes In New York',
       description:
-        'Through True Rich Attended does no end it his mother since favourable real had half every him case in packages enquire we up ecstatic.Through True Rich Attended does no end it his mother. Through True Rich Attended does no end it his mother since favourable real had half every him case in packages enquire we up ecstatic.Through True Rich Attended does no end it his mother',
+        "At our cafe, we take pride in being one of the best in New York. Our commitment to quality is evident in everything we do, from the carefully selected beans we use to make our coffee to the handmade pastries and sandwiches we serve. Our cozy atmosphere and friendly staff create a warm and welcoming environment that keeps customers coming back for more. Whether you're looking for a caffeine fix, a delicious brunch, or a cozy spot to work or catch up with friends, we've got you covered. Come see for yourself why we're one of the best cafes in New York.",
     },
   ],
   seven: [
     {
       fontSize: '70',
       color: 'white',
-      title: 'We are one of the best Cafes In New York',
-      description:
-        'Through True Rich Attended does no end it his mother since favourable real had half every him case in packages enquire we up ecstatic.Through True Rich Attended does no end it his mother. Through True Rich Attended does no end it his mother since favourable real had half every him case in packages enquire we up ecstatic.Through True Rich Attended does no end it his mother,hrough True Rich Attended does no end it his mother since favourable real had half every him case in packages enquire we up ecstatic.Through True Rich Attended does no end it his mother. Through True Rich Attended does no end it his mother since favourable real had half every him case in packages enquire we up ecstatic.Through True Rich Attended does no end it his mother,Rich Attended does no end it his mother since favourable real had half every him case in packages enquire we up ecstatic.Through True Rich Attended does no end it his mother. Through True Rich Attended does no end it his mother since favourable real had half every him case in packages enquire we up ecstatic.Through True Rich Attended does no end it his mother,hrough True Rich Attended does no end it his mother since favourable real had half every him case in packages enquire we up ecstatic.Through True Rich Attended does no end it his mother. Through True Rich Attended does no end it his mother since favourable real had half every him case in packages enquire we up ecstatic.Through True Rich Attended does no end it his mother',
+      title: 'Our story',
+      description: [
+        'Picture a cozy little restaurant, tucked away on a quiet street, with warm lighting and the aroma of freshly cooked food wafting through the air. This was the vision behind our restaurant, a place where diners could escape the hustle and bustle of everyday life and enjoy a delicious meal in a welcoming environment.',
+        'Our restaurant was built on a foundation of passion for food and a desire to create an unforgettable dining experience. We knew that people were looking for more than just a meal; they wanted a place to connect with loved ones, celebrate special occasions, and create memories that would last a lifetime.',
+        'We set out to create a restaurant that would be more than just a place to eat. We focused on creating a warm and inviting atmosphere, with thoughtful decor and attentive service. We sourced only the best ingredients and worked closely with local farmers and producers to ensure that every dish was fresh and flavorful.',
+        'But the heart of our restaurant was our menu. We wanted to offer a range of dishes that would appeal to a variety of tastes and preferences, while still showcasing our unique style and flavors. We drew inspiration from a variety of cuisines, incorporating bold spices and fresh herbs to create dishes that were both comforting and exciting.',
+        'Today, our restaurant is a beloved fixture in the community, a place where diners can come together to share a meal and create memories. It&apos;s not just a restaurant; it&apos;s a destination, a place where people come to escape, connect, and indulge in the pleasures of good food and good company.',
+      ],
     },
   ],
   eight: [
@@ -131,7 +136,7 @@ const data = {
   address: [
     {
       title: 'Openings time',
-      description: 'Sunday to Saturday',
+      description: 'Sunday to Friday',
       info: '09:00 AM to 11:00 PM',
     },
     {
@@ -142,7 +147,7 @@ const data = {
     {
       title: 'Contact us',
       description: '+123456789',
-      info: 'hey@yourdomain.com',
+      info: 'hey@example.com',
     },
   ],
   ourMenu: [
@@ -250,37 +255,37 @@ const data = {
       title: 'Quafe Cake',
       icon: '/icon(1).svg',
       description:
-        'Through True Rich Attended no end it his mother since favourable real had half every him.',
+        'Enjoy our delectable Quafe Cake, a moist and rich chocolate cake topped with velvety smooth ganache.',
     },
     {
       title: 'Coffee',
       icon: '/icon(6).svg',
       description:
-        'Through True Rich Attended no end it his mother since favourable real had half every him.',
+        'Get your caffeine dose with our premium coffee, prepared to perfection from freshly roasted beans.',
     },
     {
       title: 'Fast food',
       icon: '/icon(2).svg',
       description:
-        'Through True Rich Attended no end it his mother since favourable real had half every him.',
+        'Satisfy your appetites with our delicious and convenient fast food selections, created from the finest ingredients and served hot and ready to eat.',
     },
     {
       title: 'Cocktails',
       icon: '/icon(3).svg',
       description:
-        'Through True Rich Attended no end it his mother since favourable real had half every him.',
+        'Relax with our carefully created cocktails, handcrafted with top-shelf alcohol and the freshest ingredients for the ideal combination of flavor and relaxation.',
     },
     {
       title: 'Grill',
       icon: '/icon(4).svg',
       description:
-        'Through True Rich Attended no end it his mother since favourable real had half every him.',
+        'Experience the sizzle and flavor of our grilled dishes, cooked to perfection and served hot off the flames for a mouth-watering meal.',
     },
     {
       title: 'Snacks',
       icon: '/icon(5).svg',
       description:
-        'Through True Rich Attended no end it his mother since favourable real had half every him.',
+        'Take a break and indulge in our savory snacks, perfect for a quick bite or to share with friends. From crispy fries to cheesy nachos, we have something for every craving.',
     },
   ],
   featuresImage: ['/image1.svg', '/image2.svg'],
@@ -290,53 +295,53 @@ const data = {
   timeLine1: [
     {
       title: '10+ People',
-      subTitle: 'We are Small Team',
+      subTitle: 'Our team of dedicated professionals',
       description:
-        'Through True Rich Attended does no end it his mother since favourable.',
+        'Our restaurant app was created by a team of 10+ dedicated individuals who share a passion for food and a commitment to creating a great dining experience for our users.',
       image: '/icon.svg',
     },
     {
       title: '2014',
-      subTitle: 'We are From',
+      subTitle: 'The start of our journey',
       description:
-        'Through True Rich Attended does no end it his mother since favourable.',
+        'The year 2014 marked the beginning of our journey to create a restaurant app that would connect food lovers with the best restaurants in their area, personalized recommendations, and exclusive deals.',
       image: '/icon.svg',
     },
     {
       title: '200k',
-      subTitle: 'We Served',
+      subTitle: 'Our growing user community',
       description:
-        'Through True Rich Attended does no end it his mother since favourable.',
+        'With over 200k happy users and counting, our restaurant app has become a trusted and beloved source for finding great restaurants, trying new cuisines, and sharing dining experiences with friends and loved ones.',
       image: '/icon.svg',
     },
   ],
   timeLine2: [
     {
       title: '1998',
-      subTitle: 'Milestone One',
+      subTitle: 'The Beginning',
       description:
-        'Through True Rich Attended does no end it his mother since favourable.',
+        'Our cafe was founded in 1998, with a passion for quality coffee and delicious food.',
       image: '/icon.svg',
     },
     {
       title: '2005',
-      subTitle: 'Milestone One',
+      subTitle: 'Expansion',
       description:
-        'Through True Rich Attended does no end it his mother since favourable.',
+        'In 2005, we opened our second location, expanding our reach and growing our loyal customer base.',
       image: '/icon.svg',
     },
     {
       title: '2012',
-      subTitle: 'Milestone One',
+      subTitle: 'Innovation',
       description:
-        'Through True Rich Attended does no end it his mother since favourable.',
+        '2012 saw us introduce new and exciting menu items, including specialty drinks and handcrafted sandwiches.',
       image: '/icon.svg',
     },
     {
-      title: '2022',
-      subTitle: 'Milestone One',
+      title: '2023',
+      subTitle: 'Celebrating 26 Years',
       description:
-        'Through True Rich Attended does no end it his mother since favourable.',
+        "As we celebrate our 26th anniversary in 2023, we're grateful for our amazing staff and loyal customers who have supported us every step of the way.",
       image: '/icon.svg',
     },
   ],
